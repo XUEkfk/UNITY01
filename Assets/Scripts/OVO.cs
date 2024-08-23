9c46283c83e4d82d58d2b5332758305d2a632137
XUEkfk <love.kyngh.ky@gmail.com>
2024-08-23 15:00:26 +0800

    增加按鍵偵測A W 空白建

--- Assets/Scripts/OVO.cs
+++ Assets/Scripts/OVO.cs
@@ -13,6 +13,24 @@ public class OVO : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        {
+            // 偵測鍵盤按鍵是否被按下
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("空白鍵被按下！");
+            }
+
+            // 偵測鍵盤按鍵是否持續按住
+            if (Input.GetKey(KeyCode.W))
+            {
+                Debug.Log("W 鍵被持續按住！");
+            }
+
+            // 偵測鍵盤按鍵是否釋放
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                Debug.Log("A 鍵被釋放！");
+            }
+        }   
     }
 }
