5a37d82dd6ac2f6f679eeafe02c164f6d9513561
XUEkfk <love.kyngh.ky@gmail.com>
2024-08-23 15:13:11 +0800

    新增移動腳本(未寫完)

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb; // 2D 物理剛體
+    private Vector2 movement; // 移動向量
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // 獲取剛體組件
     }
 
     // Update is called once per frame
